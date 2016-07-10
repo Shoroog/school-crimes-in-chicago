d1e855cbfe55b9a20564c3f8c0a977342b542832
Shoroog <salhuthaifi@luc.edu>
2016-07-09 22:55:56 -0500

    Add unit tests for districts and crimes type

--- UnitTests.py
+++ UnitTests.py
@@ -1,6 +1,7 @@
 import unittest
 import CrimeNearSchool
 import os
+import csv
 
 
 class Test(unittest.TestCase):
@@ -8,11 +9,26 @@ class Test(unittest.TestCase):
     def test_pass(self):
         self.assertTrue(True)
 
-    def test_fail(self):
-        self.assertTrue(False)
+    def setUp(self):
+        self.testdata = open('Schools.csv').read()
 
-    def test_error(self):
-        raise RuntimeError('Test error!')
+    def test_type(self):
+        crimeType = []
+        with open('Crimes-2015.csv') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                crimeType.append(row[5])
+        self.assertFalse(not crimeType)
+        self.assertTrue('THEFT' in crimeType)
+
+    def test_district(self):
+        district = []
+        with open('Crimes-2015.csv') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                district.append(row[11])
+        self.assertFalse(not district)
+        self.assertTrue('011' in district)
 
 
 if __name__ == '__main__':
