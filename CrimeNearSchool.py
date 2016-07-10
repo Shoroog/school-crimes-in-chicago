3d34d64b6d63eae778b47d32cec40e0116b72d84
Shoroog <salhuthaifi@luc.edu>
2016-07-09 22:56:32 -0500

    delete the unnecessary code and organize the code

--- CrimeNearSchool.py
+++ CrimeNearSchool.py
@@ -5,67 +5,10 @@ import collections
 from collections import Counter
 
 
-# read crimes.csv
-out = open("Crimes-2015.csv","rb")
-data = csv.reader(out)
-data = [row for row in data]
-out.close()
-
-# read school.csv
-out2 = open("schools.csv","rb")
-data2 = csv.reader(out2)
-data2 = [row for row in data2]
-out2.close()
-
-dataSchoolLocation= [[row[2]]for row in data2]
-dataCrimeLocation= [[row[3]] for row in data]
-
-#writing crime location in file
-outFile= open("crime_location.csv","wb")
-output= csv.writer(outFile)
-for row in dataCrimeLocation:
-  output.writerow(row)
-outFile.close()
-
-#writing school location in file
-outFile2= open("school_location.csv","wb")
-output2= csv.writer(outFile2)
-for row in dataSchoolLocation:
-  output2.writerow(row)
-outFile2.close()
-
-# read crimes.csv
-out3 = open("crime_location.csv","rb")
-data3 = csv.reader(out3)
-data3 = [row for row in data3]
-out3.close()
-
-# read school.csv
-out4 = open("school_location.csv","rb")
-data4 = csv.reader(out4)
-data4 = [row for row in data4]
-out4.close()
-
-dataCrimeLocation2= [[row[0]] for row in data3]
-dataSchoolLocation2= [[row[0]]for row in data4]
-
-dataCrimeType= [[row[5]] for row in data]
-#writing school location in file
-outFile4= open("crime_type.csv","wb")
-output4= csv.writer(outFile4)
-for row in dataCrimeType:
-  output4.writerow(row)
-
-outFile4.close()
-
-#criminal Time
-dataCrimeTime= [[row[2]] for row in data]
-
-
 def main():
 
     crimetype()
-    timeCrime()
+    districtCrime()
 
 
 def crimetype():
@@ -73,10 +16,10 @@ def crimetype():
     os.system('clear')
     print('Crimes Type')
     crimes = []
-    with open('crime_type.csv') as f:
+    with open('Crimes-2015.csv') as f:
         reader = csv.reader(f)
         for row in reader:
-          crimes.append(row[0])
+          crimes.append(row[5])
     crimeCounts = Counter(crimes)
     x = input('Please enter the first top results by entering the amount or 0 for all data: ')
     print('The most common crimes happened in Chicago city in 2015')
@@ -90,8 +33,7 @@ def crimetype():
     os.system('clear')
 
 
-def timeCrime():
-
+def districtCrime():
 
     os.system('clear')
     print('Crimes in Specific District ')
@@ -104,7 +46,7 @@ def timeCrime():
     x = input('Please enter the first top results by entering the amount or 0 for all data: ')
     print('The most common crimes happened in these districts ')
 
-    if x==0:
+    if x == 0:
 
          for types, count in disCounts.most_common():
             print ('%s: %d times.' % (types, count))
@@ -113,7 +55,6 @@ def timeCrime():
             print ('%s: %d times.' % (types, count))
 
     os.system('clear')
-
 
 
 main()
